d151a9146ec5c862f12e9481ab633aa8d853b4dc
“Vivek <vivekbhagat1890@hotmail.com>
2023-08-17 12:05:33 +0530

    file reading input example

--- C++/File Handling/Example/example1.cpp
+++ C++/File Handling/Example/example1.cpp
@@ -6,13 +6,14 @@ using namespace std;
 
 int main()
 {
-    cout << "hello";
+
     fstream newFile;
     newFile.open("sample.txt", ios::out); // openend the file to perform the Write operation using the  file object
     if (newFile.is_open())
     {
-        newFile << "The file is opened \n"; // inserting the text
-        newFile.close();                    // closing the file
+        newFile << "The file is opened \n";                                                 // inserting the text
+        newFile << "https://vscode.dev/profile/github/aadbcac7e18b0fefb29413c4c2bbf109 \n"; // inserting the text
+        newFile.close();                                                                    // closing the file
     }
 
     newFile.open("sample.txt", ios::in); // opened the fie to do the Read operation using the file object
