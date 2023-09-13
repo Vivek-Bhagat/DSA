f752837a73a846a3eea329a60f8b69fecc27ca22
“Vivek <vivekbhagat1890@hotmail.com>
2023-09-14 01:05:21 +0530

    stack

--- C++/Stack/stack.cpp
+++ C++/Stack/stack.cpp
@@ -10,6 +10,14 @@ int main()
     stack.push(12);
     stack.push(13);
 
+    // int num so that we can add the number
+    int i, num;
+    for (i = 0; i < 5; i++)
+    {
+        cout << "Enter the number :";
+        cin >> num;
+        stack.push(num);
+    }
     while (!stack.empty())
     {
         cout << stack.top() << " ";
