6b24c48e412840aa66ee8907df5eb4efb973a4b8
Vivek <vivekbhagat1890@hotmail.com>
2023-11-09 02:16:38 +0530

    inseting at the end

--- C++/Linked List/linkedlist2.cpp
+++ C++/Linked List/linkedlist2.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+#include <string>
 #include <iostream>
 using namespace std;
 
@@ -6,7 +8,7 @@ struct node
     int data;
     struct node *next;
 };
-struct node *head = NULL;
+struct node *head;
 struct node *current = NULL;
 
 void insertAtBeg(int data)
@@ -15,6 +17,18 @@ void insertAtBeg(int data)
     p->data = data;
     p->next = head;
     head = p;
+}
+
+void insertAtEnd(int data)
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = data;
+
+    struct node *list = head;
+    while (list->next != NULL)
+        list = list->next;
+    list->next = newNode;
+    newNode->next = NULL;
 }
 
 void printList()
@@ -29,6 +43,11 @@ void printList()
 int main()
 {
     insertAtBeg(5);
+    insertAtBeg(6);
+    insertAtBeg(7);
+    printList();
+    cout << "\nAfter insertin at the end of the list" << endl;
+    insertAtEnd(10);
     printList();
     return 0;
 }
