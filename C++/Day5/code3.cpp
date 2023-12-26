062292d2e19acaafea2cbdde9b259d710ff8b50f
Vivek <vivekbhagat1890@gmail.com>
2023-12-26 16:19:42 +0530

    string conv. toUpper and toLower

--- C++/Day5/code3.cpp
+++ C++/Day5/code3.cpp
@@ -8,23 +8,29 @@ int main()
 {
     string str = "aaadsadhuhueekw";
     // conver it to upper case
-    for (int i = 0; i < str.size(); i++)
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] -= 32; // subtraction the string with 32
-        }
-    }
-    cout << " upper case conv.  :" << str << endl;
+    // for (int i = 0; i < str.size(); i++)
+    // {
+    //     if (str[i] >= 'a' && str[i] <= 'z')
+    //     {
+    //         str[i] -= 32; // subtraction the string with 32
+    //     }
+    // }
+    // cout << " upper case conv.  :" << str << endl;
 
     // converting the string to the lower case
     string str1 = "ADAHDAHADNCSEH";
-    for (int i = 0; i < str1.size(); i++)
-    {
-        if (str1[i] >= 'A' && str1[i] <= 'Z')
-            str1[i] += 32; // Adding the string with 32
-    }
-    cout << "lower case  :" << str1;
+    // for (int i = 0; i < str1.size(); i++)
+    // {
+    //     if (str1[i] >= 'A' && str1[i] <= 'Z')
+    //         str1[i] += 32; // Adding the string with 32
+    // }
+    // cout << "lower case  :" << str1;
+
+    // Now converting the string with the inbuit function
+    transform(str.begin(), str.end(), str.begin(), ::toupper); // format is transform(starting string, ending string, whereOfTheString :: functionName)
+    cout << " upper case conv.  :" << str << endl;
+    transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
+    cout << " lower case conv.  :" << str1 << endl;
 
     return 0;
 }
